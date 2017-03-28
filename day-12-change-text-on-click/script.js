0ec973400029e2e8b75204f92b0e5ffd4782ec0f
Stevie Howat <skhowat@gmail.com>
2017-03-28 16:21:19 -0500

    added day 12 change text on click assignment

---
+++ day-12-change-text-on-click/script.js
@@ -0,0 +1,5 @@
+var theParagraph = document.querySelector('.the-paragraph');
+
+theParagraph.addEventListener('click', function() {
+  theParagraph.textContent = 'Hello. My name is Inigo Montoya. You killed my father. Prepare to die.';
+});
