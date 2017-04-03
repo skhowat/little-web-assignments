40c56f5e4f136379fb0e07b6fc90d892647890a0
Stevie Howat <skhowat@gmail.com>
2017-04-03 17:12:07 -0500

    first pass at multi toggle on click assignment

---
+++ day-16-multi-toggle-on-click/script.js
@@ -0,0 +1,34 @@
+var nameOne = document.querySelector('#nameOne');
+console.log(nameOne);
+
+nameOne.addEventListener('click', function(){
+  nameOne.classList.toggle('toggle-on');
+})
+
+var nameTwo = document.querySelector('#nameTwo');
+console.log(nameTwo);
+
+nameTwo.addEventListener('click', function(){
+  nameTwo.classList.toggle('toggle-on');
+})
+
+var nameThree = document.querySelector('#nameThree');
+console.log(nameThree);
+
+nameThree.addEventListener('click', function(){
+  nameThree.classList.toggle('toggle-on');
+})
+
+var nameFour = document.querySelector('#nameFour');
+console.log(nameFour);
+
+nameFour.addEventListener('click', function(){
+  nameFour.classList.toggle('toggle-on');
+})
+
+var nameFive = document.querySelector('#nameFive');
+console.log(nameFive);
+
+nameFive.addEventListener('click', function(){
+  nameFive.classList.toggle('toggle-on');
+})
