2aaefdcbf7e6bcb2285e5e53478652bfb19b1a95
Stevie Howat <skhowat@gmail.com>
2017-04-11 17:04:38 -0500

    addd day 22 scorecard

---
+++ day-22-scorecard/script.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+
+fs.readFile('Most-Recent-Cohorts-Scorecard-Elements.csv', 'utf-8', function(err, data) {
+
+var lines = data.split('\n');
+
+for (var i=1; i<lines.length-1; i++){
+  var line = lines[i];
+  var info = line.split(',');
+  console.log(info[3] + ': ' + info[32] + '\n');
+  }
+})
